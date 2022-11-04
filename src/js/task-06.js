2369e9762a2d8589f47307e88682f2427e0ea881
ZhenyaIlchenko <108277664+ZhenyaIlchenko@users.noreply.github.com>
2022-11-04 17:09:02 +0200

    corrected task 6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -8,9 +8,12 @@ inputEl.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
   console.log(event.currentTarget.value.length);
+
   if (event.currentTarget.value.length === symbols) {
+    inputEl.classList.remove("invalid");
     inputEl.classList.add("valid");
   } else {
+    inputEl.classList.remove("valid");
     inputEl.classList.add("invalid");
   }
 }
