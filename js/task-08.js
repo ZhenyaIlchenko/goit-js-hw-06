3e48fcf8dec09d68b3d511ba8e0826a6399d2cb6
ZhenyaIlchenko <108277664+ZhenyaIlchenko@users.noreply.github.com>
2022-11-06 16:05:40 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const emailEl = event.currentTarget.elements.email;
+  const passwEl = event.currentTarget.elements.password;
+
+  if (emailEl.value === "" || passwEl === "") {
+    alert("Всі поля мають бути заповнені!");
+    return;
+  }
+  const data = {
+    email: emailEl.value,
+    password: passwEl.value,
+  };
+  console.log(data);
+
+  form.reset();
+}
