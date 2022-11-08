276667c40f191aa1dca487ba8f06c7e028c0c6b7
ZhenyaIlchenko <108277664+ZhenyaIlchenko@users.noreply.github.com>
2022-11-08 17:58:41 +0200

    corrected task 7

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,8 @@ const refs = {
   inputSize: document.querySelector("#font-size-control"),
   text: document.querySelector("#text"),
 };
+// refs.text.style.fontSize = refs.inputSize.value + "px";
+refs.inputSize.value = 16;
 
 refs.inputSize.addEventListener("input", onInputChange);
 
