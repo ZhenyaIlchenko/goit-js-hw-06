ea37c2c012cd76e706bda90534e668e07761a4ab
ZhenyaIlchenko <108277664+ZhenyaIlchenko@users.noreply.github.com>
2022-11-08 17:58:20 +0200

    corrected task 1

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,14 @@ const listEl = document.querySelectorAll(".item");
 console.log("Number of categories:", listEl.length);
 
 listEl.forEach((element) => {
-  console.log("Category:", element.querySelector("h2").textContent);
-  console.log("Elements:", element.querySelectorAll("li").length);
+  // console.log("Category:", element.querySelector("h2").textContent);
+  // console.log("Elements:", element.querySelectorAll("li").length);
+
+  console.log("Category:", element.firstElementChild.textContent);
+  console.log("Elements:", element.lastElementChild.children.length);
+
+  // let ul = element.lastElementChild;
+  // let ulChildren = ul.children;
+  // // let ulChildrenLength = ulChildren.length;
+  // console.log("Elements:", ulChildrenLength);
 });
